4011dc1880d7cf546b6e5c55a9aeb99712942751
Barthelemy Delemotte <barthelemy.delemotte@cpexterne.org>
2022-01-17 12:33:41 +0100

    fix build

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  webpack: (config, { isServer, dev }) => {
+    config.output.chunkFilename = isServer
+      ? `${dev ? "[name]" : "[name].[fullhash]"}.js`
+      : `static/chunks/${dev ? "[name]" : "[name].[fullhash]"}.js`;
+
+    return config;
+  },
 }
